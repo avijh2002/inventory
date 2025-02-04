af432b13b7f1ac4bc9912368b8b71783b52d5f1e
avijh2002 <avijh200221024@gmail.com>
2025-02-04 07:26:52 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,4 +1,4 @@
-https://inventory-uci7.vercel.app/import axios from "axios";
+import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.MODE === "development" ? "http://localhost:5004/api" : "https://inventory-uci7.vercel.app/api/",
