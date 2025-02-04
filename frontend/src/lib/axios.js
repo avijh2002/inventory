066b725b366f799c2e44df816cad95976edc6d0c
avijh2002 <avijh200221024@gmail.com>
2025-02-04 06:29:19 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5004/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5004/api" : "https://inventory-ivory-six.vercel.app/api",
   withCredentials: true,
 });
