39efe3df9732c9fcc0facb555434659baa54f1cd
Avinash Jha <avijh200221024@gmail.com>
2025-02-04 08:44:07 +0530

    pushing all code

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
-import { connectDB } from "./lib/db.js";
-import authRoutes from "./routes/auth.routes.js";
-import agentRoutes from "./routes/agent.routes.js";
-import firmRoutes from "./routes/firm.routes.js";
-import transportRoutes from "./routes/transport.routes.js";
-import qualityRoutes from "./routes/quality.routes.js";
-import orderRoutes from "./routes/order.routes.js";
+import { connectDB } from "./src/lib/db.js";
+import authRoutes from "./src/routes/auth.routes.js";
+import agentRoutes from "./src/routes/agent.routes.js";
+import firmRoutes from "./src/routes/firm.routes.js";
+import transportRoutes from "./src/routes/transport.routes.js";
+import qualityRoutes from "./src/routes/quality.routes.js";
+import orderRoutes from "./src/routes/order.routes.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import path from "path";
@@ -30,13 +30,7 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "..", "frontend", "dist", "index.html"));
-  });
-}
 
 
 app.use("/api/auth", authRoutes);
@@ -45,6 +39,16 @@ app.use("/api/firm", firmRoutes);
 app.use("/api/transport", transportRoutes);
 app.use("/api/quality", qualityRoutes);
 app.use("/api/order", orderRoutes);
+
+
+app.use(express.static(path.join(__dirname, "./frontend/dist")));
+
+
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
 
 app.listen(PORT, () => {
   console.log("Server listening on port:", PORT);
