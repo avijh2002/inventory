3cfeaa9fc12827e2ec058e58d06a706ba55ec976
avijh2002 <avijh200221024@gmail.com>
2025-02-04 07:48:52 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5004/api" : "https://inventory-uci7.vercel.app/api/",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5004/api" : "https://inventory-management-hg.vercel.app/api",
   withCredentials: true,
 });
